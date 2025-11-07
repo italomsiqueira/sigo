61aa639a04978812fe87c7a6410488b7c07598d1
italomsiqueira <italoms@live.com>
2025-11-07 07:50:58 -0300

    Menu unico agora é via include

--- turma.php
+++ turma.php
@@ -35,48 +35,10 @@ $sqlTurma = "SELECT * FROM alunos WHERE turma = $idTurma";
 </head>
 
 <body>
-    <!--menu-->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="index.php"><img src="assets/img/logo-cinza.png" width="130" height="30" alt=""></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
-            aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Alterna navegação">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <a class="nav-link" href="index.php">Início</a>
-                </li>
-
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Turma
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a class="dropdown-item" href="cadastrar-turma.php">Cadastrar turma</a>
-                        <a class="dropdown-item" href="listar-turmas.php">Ver turmas</a>
-                    </div>
-                </li>
-
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Aluno
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a class="dropdown-item" href="cadastrar-aluno.php">Cadastrar aluno</a>
-                        <a class="dropdown-item" href="listar-alunos.php">Ver alunos</a>
-                    </div>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link sair" href="login.php">Sair</a>
-                </li>
-
-            </ul>
-        </div>
-    </nav>
-    <!---------------------------------------------------------->
+    <?php
+    include('layout/menu.php');
+    ?>
+    
     <div class="container">
         <div class="row">
             <div class="col-md-12 turma">
