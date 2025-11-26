d7ca8a286fa5ffe943e53c919deff311e9e02297
italomsiqueira <italoms@live.com>
2025-11-26 08:19:32 -0300

    Teste

--- hash.php
+++ hash.php
@@ -1,3 +1,3 @@
 <?php
-echo password_hash('insira a senha que deseja converter em hash', PASSWORD_DEFAULT);
+echo password_hash('Teste insira a senha que deseja converter em hash', PASSWORD_DEFAULT);
 ?>
